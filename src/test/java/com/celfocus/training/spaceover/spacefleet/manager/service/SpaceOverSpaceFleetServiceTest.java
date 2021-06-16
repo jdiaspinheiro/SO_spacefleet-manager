da6e59dbb23e0a21aa0ec299316796c3da38c9bc
João Dias Pinheiro <NB27498@novabase.pt>
2021-06-16 14:35:12 +0100

    SpaceFleetServiceTest for review v0.1

--- src/test/java/com/celfocus/training/spaceover/spacefleet/manager/service/SpaceOverSpaceFleetServiceTest.java
+++ src/test/java/com/celfocus/training/spaceover/spacefleet/manager/service/SpaceOverSpaceFleetServiceTest.java
@@ -9,6 +9,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
@@ -22,38 +24,81 @@ class SpaceOverSpaceFleetServiceTest {
     private SpaceOverSpaceFleetService underTest;
 
     @Test
+    @DisplayName("GIVEN a List of spacefleets that should be found, WHEN I run the find all method, " +
+            "THEN I expect the found List of spacefleets to be equal to the list of spacefleets that should be found")
     void findAll() {
-        // when
-        underTest.findAll();
-        //then
-        verify(spaceFleetRepository, times(1)).findAll();
+        // arrange
+        List<SpaceFleet> spaceFleetsToFind = new ArrayList<SpaceFleet>() {
+            {
+                add(SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build());
+                add(SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build());
+                add(SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build());
+                add(SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build());
+            }
+        };
+
+        when(spaceFleetRepository.findAll()).thenReturn(spaceFleetsToFind);
+
+        // act
+        List<SpaceFleet> spaceFleetsFound = underTest.findAll();
+
+        // assert
+        assertEquals(spaceFleetsFound, spaceFleetsToFind);
     }
 
     @Test
+    @DisplayName("Given X When I DO Y, I expect Z")
     void findBydId() {
+        // arrange
+        SpaceFleet spaceFleetToFind = SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build();
+
+        when(spaceFleetRepository.findById(1L)).thenReturn(Optional.of(spaceFleetToFind));
+
+        // act
+        SpaceFleet spaceFleetFoundByID = underTest.findBydId(1L);
+
+        // assert
+        assertEquals(spaceFleetFoundByID, spaceFleetToFind);
     }
 
     @Test
     @DisplayName("Given X When I DO Y, I expect Z")
     void save() {
         // arrange
-        SpaceFleet spaceFleetBeforePersistence = SpaceFleet.builder().name("nome").sector("sector").status("status").build();
-        SpaceFleet spaceFleetAfterPersistence = SpaceFleet.builder().id(1L).name("nome").sector("sector").status("status").build();
+        SpaceFleet spaceFleetBeforePersistence = SpaceFleet.builder().name("name").sector("sector").status("status").build();
+        SpaceFleet spaceFleetAfterPersistence = SpaceFleet.builder().id(1L).name("name").sector("sector").status("status").build();
 
         when(spaceFleetRepository.save(spaceFleetBeforePersistence)).thenReturn(spaceFleetAfterPersistence);
 
         // act
-        SpaceFleet spaceFleetRepositoryTestById = underTest.save(spaceFleetBeforePersistence);
+        SpaceFleet spaceFleetSaved = underTest.save(spaceFleetBeforePersistence);
 
         // assert
-        assertEquals(spaceFleetAfterPersistence, spaceFleetRepositoryTestById);
+        assertEquals(spaceFleetAfterPersistence, spaceFleetSaved);
     }
 
     @Test
+    @DisplayName("Given X When I DO Y, I expect Z")
     void update() {
+        // arrange
+        SpaceFleet spaceFleetAfterUpdate = SpaceFleet.builder().id(1L).name("name2").sector("sector2").status("status2").build();
+
+        when(spaceFleetRepository.save(spaceFleetAfterUpdate)).thenReturn(spaceFleetAfterUpdate);
+
+        // act
+        SpaceFleet spaceFleetSaved = underTest.update(spaceFleetAfterUpdate);
+
+        // assert
+        assertEquals(spaceFleetAfterUpdate, spaceFleetSaved);
     }
 
     @Test
+    @DisplayName("Given X When I DO Y, I expect Z")
     void deleteById() {
+        // act
+        underTest.deleteById(1L);
+
+        // assert
+        verify(spaceFleetRepository).deleteById(1L);
     }
 }
